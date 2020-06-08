14bb5fe29dcf1599c72f8e9df8a8d273c50ed53b
unknown <hpcarg@gmail.com>
2020-06-08 00:43:15 -0300

    poke-model

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -2,47 +2,14 @@ const mongoose = require('mongoose');
 const BaseSchema = require('./base');
 
 /**
- * task model schema (simple example).
+ * pokemon schema.
  */
-const taskSchema = new BaseSchema({
+const pokemonSchema = new BaseSchema({
     name: {
         type: String,
-        required: [true, 'task-name-required']
+        required: true
     },
-    description: { type: String, required: false },
-    consumableName: { type: String, required: false },
-    consumableId: { type: String, required: false },
-    consumableAmount: {
-        type: Number,
-        required: false,
-        cast: 'task-consumableAmount-invalid'
-    },
-    workForceAmount: {
-        type: Number,
-        required: false,
-        cast: 'task-workForceAmount-invalid'
-    },
-    workForceHours: {
-        type: Number,
-        required: false,
-        cast: 'task-workForceHours-invalid'
-    },
-    type: { type: String, required: false },
-    maintenancePlans: { type: String, required: false },
-    workForceFee: { type: Number, required: false },
-    consumableFee: { type: Number, required: false },
-    consumableAmountPlusFee: { type: Number, required: false },
-    workForceAmountPlusFee: { type: Number, required: false },
-    clientGroup: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'clientGroup',
-        required: false
-    },
-    organization: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'organization',
-        required: false
-    }
+    description: { type: String, required: false }
 });
 
-module.exports = mongoose.model('task', taskSchema);
+module.exports = mongoose.model('pokemon', pokemonSchema);
